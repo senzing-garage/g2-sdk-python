9776d0c0c6191507fb34d91a8e76224ee707bb19
michael@dockter.com <michael@dockter.com>
2021-11-18 18:52:17 -0500

    #3 Senzing 1.2.0

--- tests/test-imports.py
+++ tests/test-imports.py
@@ -8,7 +8,10 @@
 
 from senzing import *
 
-# xx = G2Module.G2Module()
+# g2_audit = G2Audit.G2Audit()
+# g2_config = G2Config.G2Config()
+# g2_engine = G2Engine.G2Engine()
+# g2_product = G2Product.G2Product()
 
 print("Test 1: Success")
 
@@ -16,9 +19,10 @@ print("Test 1: Success")
 
 from senzing import G2Audit, G2Config, G2Engine, G2Exception, G2Product
 
-# xx = G2Module.G2Module("x", "y")
-
-# xx = G2Module.G2Module("x", "y")
+# g2_audit = G2Audit.G2Audit()
+# g2_config = G2Config.G2Config()
+# g2_engine = G2Engine.G2Engine()
+# g2_product = G2Product.G2Product()
 
 print("Test 2: Success")
 
