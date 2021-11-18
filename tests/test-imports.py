97ced928ea2603bf657a279e9ddbf4edd2801ce1
michael@dockter.com <michael@dockter.com>
2021-11-18 17:53:30 -0500

    #3 Update test cases

--- tests/test-imports.py
+++ tests/test-imports.py
@@ -4,18 +4,19 @@
 # Test importing libraries.
 # -----------------------------------------------------------------------------
 
-
 # ----- Import all - implicitly -----------------------------------------------
 
 from senzing import *
 
-xx = G2Module.G2Module()
+# xx = G2Module.G2Module()
 
 print("Test 1: Success")
 
 # ----- Import all - explicitly -----------------------------------------------
 
 from senzing import G2Exception, G2Module
+
+xx = G2Module.G2Module("x", "y")
 
 print("Test 2: Success")
 
@@ -26,12 +27,6 @@ from senzing.G2Exception import G2InvalidFileTypeContentsException as bob
 import senzing.G2Exception
 
 print("Test 3: Success")
-
-# ----- Import from G2Exception -----------------------------------------------
-
-xx1 = senzing.G2Module()
-
-print("Test 4: Success")
 
 # ----- Epilog ----------------------------------------------------------------
 
