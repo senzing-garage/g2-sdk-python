14c4c3e8b2536712d81675f1bd678d496e19d394
michael@dockter.com <michael@dockter.com>
2022-03-30 13:12:54 -0400

    #94 Savepoint

--- src/senzing/G2Exception.py
+++ src/senzing/G2Exception.py
@@ -1,11 +1,6 @@
 __all__ = [
-    'G2DBException',
-    'G2DBMNotStarted',
-    'G2DBNotFound',
-    'G2DBUniqueConstraintViolation',
-    'G2DBUnknownException',
+    'G2BadInputException',
     'G2Exception',
-    'G2InvalidFileTypeContentsException',
     'G2ModuleEmptyMessage',
     'G2ModuleException',
     'G2ModuleGenericException',
@@ -14,144 +9,176 @@ __all__ = [
     'G2ModuleMySQLNoSchema',
     'G2ModuleNotInitialized',
     'G2ModuleResolveMissingResEnt',
-    'G2TableNoExist',
-    'G2UnsupportedDatabaseType',
-    'G2UnsupportedFileTypeException',
-    'TranslateG2ModuleException',
-    'UnconfiguredDataSourceException'
-    ]
+    'G2RetryableException',
+    'G2UnrecoverableException',
+    'TranslateG2ModuleException'
+]
+
+# -----------------------------------------------------------------------------
+# Base G2Exception
+# -----------------------------------------------------------------------------
 
 
 class G2Exception(Exception):
     '''Base exception for G2 related python code'''
 
     def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
+
+# -----------------------------------------------------------------------------
+# Category exceptions
+# - These exceptions represent categories of actions that can be taken by
+#   the calling program.
+# - G2BadInputException - The user-supplied input contained an error.
+# - G2RetryableException - The program can provide a remedy and continue.
+# - G2UnrecoverableException - System failure; can't continue.
+# -----------------------------------------------------------------------------
 
 
-class G2UnsupportedFileTypeException(G2Exception):
-
-    def __init__(self, *args, **kwargs):
-        G2Exception.__init__(self, *args, **kwargs)
-
-
-class G2InvalidFileTypeContentsException(G2Exception):
-
-    def __init__(self, *args, **kwargs):
-        G2Exception.__init__(self, *args, **kwargs)
-
-
-class G2DBException(G2Exception):
-    '''Base exception for G2 DB related python code'''
+class G2BadInputException(G2Exception):
 
     def __init__(self, *args, **kwargs):
-        G2Exception.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
 
 
-class UnconfiguredDataSourceException(G2Exception):
-
-    def __init__(self, DataSourceName):
-        G2Exception.__init__(self, ("Datasource %s not configured. See https://senzing.zendesk.com/hc/en-us/articles/360010784333 on how to configure datasources in the config file." % DataSourceName))
-
-
-class G2DBUnknownException(G2DBException):
+class G2RetryableException(G2Exception):
 
     def __init__(self, *args, **kwargs):
-        G2DBException.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
 
 
-class G2UnsupportedDatabaseType(G2DBException):
-
-    def __init__(self, *args, **kwargs):
-        G2DBException.__init__(self, *args, **kwargs)
-
-
-class G2TableNoExist(G2DBException):
+class G2UnrecoverableException(G2Exception):
 
     def __init__(self, *args, **kwargs):
-        G2DBException.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
+
+# -----------------------------------------------------------------------------
+# Detail exceptions for G2BadInputException
+# - Processing did not complete.
+# - These exceptions are "per record" exceptions.
+# - The record should be recorded as "bad".  (logged, queued as failure)
+# - Processing may continue.
+# -----------------------------------------------------------------------------
 
 
-class G2DBMNotStarted(G2DBException):
-
-    def __init__(self, *args, **kwargs):
-        G2DBException.__init__(self, *args, **kwargs)
-
-
-class G2DBNotFound(G2DBException):
-
-    def __init__(self, *args, **kwargs):
-        G2DBException.__init__(self, *args, **kwargs)
-
-
-class G2DBUniqueConstraintViolation(G2DBException):
+class G2IncompleteRecordException(G2BadInputException):
 
     def __init__(self, *args, **kwargs):
-        G2DBException.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
+
+
+class G2MalformedJsonException(G2BadInputException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+
+class G2MissingConfigurationException(G2BadInputException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+
+class G2MissingDataSourceException(G2BadInputException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+# -----------------------------------------------------------------------------
+# Detail exceptions for G2RetryableException
+# - Processing did not complete.
+# - These exceptions may be remedied programmatically.
+# - The call to the Senzing method should be retried.
+# - Processing may continue.
+# -----------------------------------------------------------------------------
+
+class G2MessageBufferException(G2RetryableException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+# -----------------------------------------------------------------------------
+# Detail exceptions for G2UnrecoverableException
+# - Processing did not complete.
+# - These exceptions cannot be remedied programmatically.
+# - Processing cannot continue.
+# -----------------------------------------------------------------------------
 
 
 class G2ModuleException(G2Exception):
     '''Base exception for G2 Module related python code'''
 
     def __init__(self, *args, **kwargs):
-        G2Exception.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
 
 
-class G2ModuleNotInitialized(G2ModuleException):
-    '''G2 Module called but has not been initialized '''
+class G2ModuleEmptyMessage(G2ModuleException):
 
     def __init__(self, *args, **kwargs):
-        G2ModuleException.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
 
 
 class G2ModuleGenericException(G2ModuleException):
     '''Generic exception for non-subclassed G2 Module exception '''
 
     def __init__(self, *args, **kwargs):
-        G2ModuleException.__init__(self, *args, **kwargs)
-
-
-class G2ModuleMySQLNoSchema(G2ModuleException):
-
-    def __init__(self, *args, **kwargs):
-        G2ModuleException.__init__(self, *args, **kwargs)
-
-
-class G2ModuleEmptyMessage(G2ModuleException):
-
-    def __init__(self, *args, **kwargs):
-        G2ModuleException.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
 
 
 class G2ModuleInvalidXML(G2ModuleException):
 
     def __init__(self, *args, **kwargs):
-        G2ModuleException.__init__(self, *args, **kwargs)
-
-
-class G2ModuleResolveMissingResEnt(G2ModuleException):
-
-    def __init__(self, *args, **kwargs):
-        G2ModuleException.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
 
 
 class G2ModuleLicenseException(G2ModuleException):
 
     def __init__(self, *args, **kwargs):
-        G2ModuleException.__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
 
 
-def TranslateG2ModuleException(ex):
-    exInfo = ex.decode().split('|', 1)
-    if exInfo[0] == '7213E':
-        return G2ModuleMySQLNoSchema(ex)
-    elif exInfo[0] == '0002E':
-        return G2ModuleInvalidXML(ex.decode())
-    elif exInfo[0] == '0007E':
-        return G2ModuleEmptyMessage(ex.decode())
-    elif exInfo[0] == '2134E':
-        return G2ModuleResolveMissingResEnt(ex.decode())
-    elif exInfo[0] == '9000E':
-        return G2ModuleLicenseException(ex.decode())
-    else:
-        return G2ModuleGenericException(ex.decode())
+class G2ModuleMySQLNoSchema(G2ModuleException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+
+class G2ModuleNotInitialized(G2ModuleException):
+    '''G2 Module called but has not been initialized '''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+
+class G2ModuleResolveMissingResEnt(G2ModuleException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+# -----------------------------------------------------------------------------
+# Determine Exception based on Senzing reason code.
+# Reference: https://senzing.zendesk.com/hc/en-us/articles/360026678133-Engine-Error-codes
+# -----------------------------------------------------------------------------
+
+
+exceptions_map = {
+    "0002E": G2ModuleInvalidXML,
+    "0007E": G2ModuleEmptyMessage,
+    "2134E": G2ModuleResolveMissingResEnt,
+    "7213E": G2ModuleMySQLNoSchema,
+    "9000E": G2ModuleLicenseException,
+
+    "30121E": G2MalformedJsonException,
+    "30122E": G2MalformedJsonException,
+
+    "30110E": G2MessageBufferException,
+    "30112E": G2MessageBufferException,
+
+
+}
+
+
+def TranslateG2ModuleException(exception_message):
+    senzing_error_code = exception_message.split('|', 1)[0].strip()
+    senzing_error_class = exceptions_map.get(senzing_error_code, G2Exception)
+    return senzing_error_class(exception_message)
