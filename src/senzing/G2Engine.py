5115f49893b2daf444dc0c4bf47bbdbc7d1822cf
jae <jae@senzing.com>
2023-06-22 11:27:21 -0600

    #162 Adding new API call for searching with search-profiles.

--- src/senzing/G2Engine.py
+++ src/senzing/G2Engine.py
@@ -817,6 +817,31 @@ class G2Engine(object):
 
         response += tls_var.buf.value
 
+    def searchByAttributesV3(self, jsonData, searchProfile, response, flags=G2EngineFlags.G2_SEARCH_BY_ATTRIBUTES_DEFAULT_FLAGS, *args, **kwargs):
+        # type: (str,str,bytearray) -> int
+        """ Find records matching the provided attributes
+        Args:
+            jsonData: A JSON document containing the attribute information to search.
+            searchProfile: A search profile name.
+            flags: control flags.
+            response: A bytearray for returning the response document; if an error occurred, an error response is stored here.
+        """
+        response[::] = b''
+        _jsonData = self.prepareStringArgument(jsonData)
+        _searchProfile = self.prepareStringArgument(searchProfile)
+        responseBuf = c_char_p(addressof(tls_var.buf))
+        responseSize = c_size_t(tls_var.bufSize)
+        self._lib_handle.G2_searchByAttributes_V3.argtypes = [c_char_p, c_char_p, c_longlong, POINTER(c_char_p), POINTER(c_size_t), self._resize_func_def]
+        ret_code = self._lib_handle.G2_searchByAttributes_V3(_jsonData, _searchProfile, flags, pointer(responseBuf), pointer(responseSize), self._resize_func)
+
+        if ret_code == -1:
+            raise G2ModuleNotInitialized('G2Engine has not been successfully initialized')
+        elif ret_code < 0:
+            self._lib_handle.G2_getLastException(tls_var.buf, sizeof(tls_var.buf))
+            raise TranslateG2ModuleException(tls_var.buf.value)
+
+        response += tls_var.buf.value
+
     @deprecated(1005)
     def findPathByEntityIDV2(self, startEntityID, endEntityID, maxDegree, flags, response):
         self.findPathByEntityID(startEntityID, endEntityID, maxDegree, response, flags)
